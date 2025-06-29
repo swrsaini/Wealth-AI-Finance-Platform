287bb8725e6fa06368313a723525dab2107be534
Shubham Saini <Shubhamsaini98465@gmail.com>
2025-06-29 15:43:42 +0530

    created a DB Model

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,14 @@
+
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
 import { LayoutDashboard, PenBox } from 'lucide-react'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async() => {
+  await checkUser();
   return (
     <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
       <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
